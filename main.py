a6ab233b817804b88426b49cc94e080cd70a4438
Benjamin Harvey <benjaminharvey2000@gmail.com>
2025-04-21 10:08:57 +0930

    Ability to draw line on canvas

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 from tkinter import Tk, BOTH, Canvas
 
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point1:Point, point2:Point):
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self, canvas:Canvas, fillColour):
+        canvas.create_line(
+            self.point1.x, 
+            self.point1.y,
+            self.point2.x,
+            self.point2.y, 
+            fill=fillColour, 
+            width=2
+        )
+    
 class Window:
     def __init__(self, width, height):
         self.root = Tk()
@@ -9,6 +29,8 @@ class Window:
         self.is_running = False
         self.root.protocol("WM_DELETE_WINDOW", self.close)
 
+    def draw_line(self, line:Line, fillColour):
+        line.draw(self.canvas, fillColour)
 
     def redraw(self):
         self.root.update_idletasks()
@@ -22,8 +44,10 @@ class Window:
     def close(self):
         self.is_running = False
 
+
 def main():
     win = Window(800,600)
+    win.draw_line(Line(Point(0,0), Point(25,25)), "black")
     win.wait_for_close()
 
 if __name__ == "__main__":
